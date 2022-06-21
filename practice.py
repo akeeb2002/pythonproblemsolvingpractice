70a367d6e7a799ebc8d9322990510dc3ff5272bb
Akeeb Khandakar <90944924+akeeb2002@users.noreply.github.com>
2022-06-21 18:59:38 -0400

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,13 @@
+x = [4,3,2,1,1,2,6]
+print ("The original list is : " +  str(x))
+  
+y = []
+def remove_duplicates(x):
+  for i in x:
+      if i not in y:
+          y.append(i)
+  y.sort()
+  print ("List after removing duplicates : " + str(y))
+
+
+remove_duplicates(x) 
